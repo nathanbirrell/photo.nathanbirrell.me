4264d3272660074562e9c616689a71fa521e0253
Nathan Birrell <nathanbirrell@gmail.com>
2014-08-10 13:30:39 +1000

    Image styling for 100% viewport

--- site/templates/default.php
+++ site/templates/default.php
@@ -24,15 +24,35 @@
       </div>
     </div>
 
-    <div class="medium-9 columns panel">
+    <div class="medium-9 columns panel post">
       <?php
         $post = $pages->find('a')->children->last();
         $date = $post->date('d M, Y');
-
-        echo kirbytext($post->title());
-        echo kirbytext($date);
-        echo kirbytext($post->text());
       ?>
+
+      <h1><?php echo $post->title() ?></h1>
+
+      <span class="date"><?php echo $date ?></span>
+
+      <?php echo kirbytext($post->text()) ?>
+
+      <?php foreach($post->images() as $img): ?>
+        <?php
+          $w = $img->width();
+          $h = $img->height();
+          $tall = "";
+          if ($h > $w) {
+            $tall = "tall";
+          }
+        ?>
+        <div class="full-image">
+          <img
+            src="<?php echo $img->url() ?>"
+            alt="<?php echo $img->name() ?>"
+            class="<?php echo $tall ?>"
+          />
+        </div>
+      <?php endforeach ?>
     </div>
 
 </div>
