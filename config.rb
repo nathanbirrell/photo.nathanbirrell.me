da746d779d118b27a3fbe79aa3852a4bd189e3a3
Nathan Birrell <nathanbirrell@gmail.com>
2014-08-10 16:07:03 +1000

    Images not right on mobile, attempting to fix

--- config.rb
+++ config.rb
@@ -3,6 +3,7 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 add_import_path "bower_components/foundation/scss"
+add_import_path "compass"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
