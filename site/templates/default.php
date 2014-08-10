1dd0f9d1bb0afdf4df6c28a2a3fdb66837bad6e7
Nathan Birrell <nathanbirrell@gmail.com>
2014-08-10 13:57:16 +1000

    Fixed disappearing images

--- site/templates/default.php
+++ site/templates/default.php
@@ -24,36 +24,9 @@
       </div>
     </div>
 
-    <div class="medium-9 columns panel post">
-      <?php
-        $post = $pages->find('a')->children->last();
-        $date = $post->date('d M, Y');
-      ?>
 
-      <h1><?php echo $post->title() ?></h1>
+    <?php snippet('post-images') ?>
 
-      <span class="date"><?php echo $date ?></span>
-
-      <?php echo kirbytext($post->text()) ?>
-
-      <?php foreach($post->images() as $img): ?>
-        <?php
-          $w = $img->width();
-          $h = $img->height();
-          $tall = "";
-          if ($h > $w) {
-            $tall = "tall";
-          }
-        ?>
-        <div class="full-image">
-          <img
-            src="<?php echo $img->url() ?>"
-            alt="<?php echo $img->name() ?>"
-            class="<?php echo $tall ?>"
-          />
-        </div>
-      <?php endforeach ?>
-    </div>
 
 </div>
 
