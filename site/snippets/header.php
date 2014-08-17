dbf9597d74157fdf8b2561c60ded3e2b1aff80b5
Nathan Birrell <nathanbirrell@gmail.com>
2014-08-17 13:40:50 +1000

    added favicons

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -9,6 +9,12 @@
   <meta name="keywords" content="<?php echo html($site->keywords()) ?>" />
   <meta name="robots" content="index, follow" />
 
+  <link rel="shortcut icon" href="assets/img/favicon.ico">
+  <link rel="apple-touch-icon" href="assets/img/apple-touch-icon.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="assets/img/apple-touch-icon-72x72-precomposed.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="assets/img/apple-touch-icon-114x114-precomposed.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="assets/img/apple-touch-icon-144x144-precomposed.png">
+
   <?php echo css('assets/styles/app.css') ?>
   <?php echo js('bower_components/modernizr/modernizr.js') ?>
 
