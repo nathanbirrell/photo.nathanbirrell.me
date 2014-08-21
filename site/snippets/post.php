1f31b699fde13bb3c36662a471fd3d4891501023
Nathan Birrell <nathanbirrell@gmail.com>
2014-08-21 19:38:56 +1000

    added google+ author

--- site/snippets/post.php
+++ site/snippets/post.php
@@ -2,6 +2,8 @@
 
   <article class="medium-9 medium-offset-3 columns panel post">
 
+    <a href="https://plus.google.com/116914196395616585208? rel=author" class="hide">+</a>
+
     <?php
       if ($page->isHomePage()) {
         $post = $pages->find('a')->children->last();
